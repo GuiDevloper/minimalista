c509ddfc80de1b009cfba0f8c240c36ad0b2bd28
GuiDevloper <guicorrea384@gmail.com>
2019-03-31 22:38:45 -0300

    added color manipulation

--- src.js
+++ src.js
@@ -55,3 +55,75 @@
     document.documentElement.clientWidth ||
     document.body.clientWidth;
   }
+
+  export var minCon = {
+    aa: 4.5,
+    aaLarge: 3,
+    aaa: 7,
+    aaaLarge: 4.5
+  };
+
+  export function genColor(bg, rgb) {
+    let newColor = "";
+    // Percorre R G B
+    for (let i = 0; i < 3; i++) {
+      let newR = rgb[i];
+      let actualCon = 0,
+        maxCont = 0,
+        bstColor = 0;
+      // busca melhor contraste
+      for (let j = 0; j < 255; j++) {
+        newR = newR < 255 ? ++newR : 0;
+        let nRgb = Array.from(rgb);
+        nRgb.splice(i, 1, newR);
+        actualCon = contrast(bg, nRgb);
+        const isBetter = actualCon > maxCont;
+        bstColor = isBetter ? newR : bstColor;
+        maxCont = isBetter ? actualCon : maxCont;
+        if (actualCon > minCon.aa) {
+          break;
+        }
+      }
+      rgb[i] = bstColor;
+      bstColor = (+bstColor).toString(16);
+      newColor += bstColor.length < 2 ? '0' + bstColor : bstColor;
+    }
+    // retorna hex constrastado
+    return newColor;
+  }
+
+  export function getRgb(hex) {
+    const h = hex.replace('#', '').split('');
+    function pI(s) {
+      return parseInt(s, 16);
+    }
+    return [
+      pI(h[0] + h[1]),
+      pI(h[2] + h[3]),
+      pI(h[4] + h[5])
+    ];
+  }
+
+  export function luminos(rgb) {
+    // http://www.w3.org/TR/WCAG20/#relativeluminancedef
+    var lum = [];
+    for (var i = 0; i < rgb.length; i++) {
+      var chan = rgb[i] / 255;
+      lum[i] = chan <= 0.03928 ?
+        chan / 12.92 :
+          Math.pow((chan + 0.055) / 1.055, 2.4);
+    }
+    return 0.2126 * lum[0] +
+      0.7152 * lum[1] +
+      0.0722 * lum[2];
+  }
+
+  export function contrast(bg, color2) {
+    // http://www.w3.org/TR/WCAG20/#contrast-ratiodef
+    var lum1 = luminos(bg);
+    var lum2 = luminos(color2);
+    if (lum1 > lum2) {
+      return (lum1 + 0.05) / (lum2 + 0.05);
+    }
+    return (lum2 + 0.05) / (lum1 + 0.05);
+  }
